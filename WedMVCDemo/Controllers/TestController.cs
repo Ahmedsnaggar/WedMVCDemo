0541d4ccf09bd10e3723d5d118cf3fe900a9874e
ahmed naggar <ahmed_s_naggar@hotmail.com>
2024-09-12 03:16:57 +0300

    Add project files.

---
+++ WedMVCDemo/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WedMVCDemo.Controllers
+{
+    public class TestController : Controller
+    {    
+        public IActionResult Index()
+        {
+            int[] ints = { 1, 2, 3 };
+            ViewData["userName"] = "User";
+            ViewBag.userName = "Admin";
+            ViewBag.numbers = ints;
+            ViewData["MyName"] = "Ahmed";
+           
+            return View();
+        }
+        [Route("calculate/{num?}")]
+        public int Calc(int num = 50)
+        {
+            return num;
+        }
+    }
+}
