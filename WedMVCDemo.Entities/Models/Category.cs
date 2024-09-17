a87364b96fddaff7d99165507f0b6a32234783ed
ahmed naggar <ahmed_s_naggar@hotmail.com>
2024-09-17 21:54:41 +0300

    Lec17/09/2024

--- WedMVCDemo.Entities/Models/Category.cs
+++ WedMVCDemo.Entities/Models/Category.cs
@@ -7,8 +7,10 @@ namespace WedMVCDemo.Entities.Models
         [Key]
         public int Id { get; set; }
         [StringLength(50)]
+        [Display(Name ="Category name")]
         public string CategoryName { get; set; }
         [StringLength(150)]
+        [Display(Name = "Category description")]
         public string? CategoryDescription { get; set; }
     }
 }
